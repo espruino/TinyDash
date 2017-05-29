d6cc814bd9b32e8c9f75f972c2799dc12307d3e4
Gordon Williams <gw@pur3.co.uk>
2017-05-29 10:28:24 +0100

    allow value setting

--- tinydash.js
+++ tinydash.js
@@ -29,31 +29,50 @@ var TD = {
   TD.label = function(opts) {
     return setup(opts,toElement('<div class="td td_label"><span>Hello</span></div>'));
   };
-  /* {label, glyph}*/
+  /* {label, glyph, value}*/
   TD.button= function(opts) {
+    var pressed = opts.value?1:0;
     opts.glyph = opts.glyph || "&#x1f4a1;";
-    var b = setup(opts,toElement('<div class="td td_btn" pressed="1"><span>'+opts.label+'</span><div class="td_btn_a">'+opts.glyph+'</div></div>'));
-    b.getElementsByClassName("td_btn_a")[0].onclick = function() {
-      togglePressed(b);
+    var el = setup(opts,toElement('<div class="td td_btn" pressed="'+pressed+'"><span>'+opts.label+'</span><div class="td_btn_a">'+opts.glyph+'</div></div>'));
+    el.getElementsByClassName("td_btn_a")[0].onclick = function() {
+      togglePressed(el);
     };
-    return b;
+    el.setValue = function(v) {
+      el.pressed = v?1:0;
+      el.setAttribute("pressed", el.pressed);
+    };
+    return el;
   };
-  /* {label}*/
+  /* {label,value}*/
   TD.toggle= function(opts) {
-    var t = setup(opts,toElement('<div class="td td_toggle"><span>'+opts.label+'</span><div class="td_toggle_a"><div class="td_toggle_b"/></div></div>'));
-    t.getElementsByClassName("td_toggle_a")[0].onclick = function() {
-      togglePressed(t);
+    var pressed = opts.value?1:0;
+    var el = setup(opts,toElement('<div class="td td_toggle" pressed="'+pressed+'"><span>'+opts.label+'</span><div class="td_toggle_a"><div class="td_toggle_b"/></div></div>'));
+    el.getElementsByClassName("td_toggle_a")[0].onclick = function() {
+      togglePressed(el);
     };
-    return t;
+    el.setValue = function(v) {
+      el.pressed = v?1:0;
+      el.setAttribute("pressed", el.pressed);
+    };
+    return el;
   };
-  /* {label}*/
+  /* {label,value}*/
   TD.value= function(opts) {
-    return setup(opts,toElement('<div class="td td_val"><span>'+opts.label+'</span><div class="td_val_a">??</div></div>'));
+    var v = (opts.value===undefined)?"?":opts.value;
+    var el = setup(opts,toElement('<div class="td td_val"><span>'+opts.label+'</span><div class="td_val_a">'+v+'</div></div>'));
+    el.setValue = function(v) {
+      el.getElementsByClassName("td_val_a")[0].innerHTML = v;
+    };
+    return el;
   };
-  /* {label}*/
+  /* {label,value,min,max}*/
   TD.guage= function(opts) {
-    var g = setup(opts,toElement('<div class="td td_guage"><span>'+opts.label+'</span><canvas></canvas><div class="td_guage_a">??</div></div>'));
-    var c = g.getElementsByTagName("canvas")[0];
+    var v = (opts.value===undefined)?0:opts.value;
+    var min = (opts.min===undefined)?0:opts.min;
+    var max = (opts.max===undefined)?1:opts.max;
+    var el = setup(opts,toElement('<div class="td td_guage"><span>'+opts.label+'</span><canvas></canvas><div class="td_guage_a">'+v+'</div></div>'));
+    el.value = v;
+    var c = el.getElementsByTagName("canvas")[0];
     var ctx = c.getContext("2d");
     function draw() {
       c.width = c.clientWidth;
@@ -69,18 +88,25 @@ var TD = {
       ctx.beginPath();
       ctx.lineWidth=16;
       ctx.strokeStyle = LIGHTCOL;
-      var v = 0.2;
+      var v = (el.value-min) / (max-min);
+      if (v<0) v=0;
+      if (v>1) v=1;
       ctx.arc(s/2, s/2+20, (s/2)-24, Math.PI*0.75, (0.75+(1.5*v))*Math.PI);
       ctx.stroke();
     }
     setTimeout(draw,100);
-    g.onresize = draw;
-    return g;
+    el.onresize = draw;
+    el.setValue = function(v) {
+      el.value = v;
+      el.getElementsByClassName("td_guage_a")[0].innerHTML = v;
+      draw();
+    };
+    return el;
   };
   /* {label}*/
   TD.graph= function(opts) {
-    var g = setup(opts,toElement('<div class="td td_graph"><span>'+opts.label+'</span><canvas></canvas></div>'));
-    var c = g.getElementsByTagName("canvas")[0];
+    var el = setup(opts,toElement('<div class="td td_graph"><span>'+opts.label+'</span><canvas></canvas></div>'));
+    var c = el.getElementsByTagName("canvas")[0];
     var ctx = c.getContext("2d");
     function draw() {
       c.width = c.clientWidth;
@@ -109,13 +135,14 @@ var TD = {
       ctx.stroke();
     }
     setTimeout(draw,100);
-    g.onresize = draw;
-    return g;
+    el.onresize = draw;
+    return el;
   };
   /* {label}*/
   TD.log= function(opts) {
     var lines = ["This is a test","of logging","in multiple lines"];
     for (var i=0;i<10;i++) lines.push(i);
-    return setup(opts,toElement('<div class="td td_log"><span>'+opts.label+'</span><div class="td_log_a td_scrollable">'+lines.join("<br/>\n")+'</div></div>'));
+    var el = setup(opts,toElement('<div class="td td_log"><span>'+opts.label+'</span><div class="td_log_a td_scrollable">'+lines.join("<br/>\n")+'</div></div>'));
+    return el;
   };
 })();
